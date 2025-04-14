4f28ff187ee29bff6b81a13ca03dd8876d18b570
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2025-04-13 21:35:44 -0500

    Working on adding file table

--- src/scan.rs
+++ src/scan.rs
@@ -34,8 +34,17 @@ pub(crate) fn add_paths(
     input_directory: &Utf8PathBuf,
 ) -> Result<(), MsiError> {
     info!("Getting paths to include in the MSI");
-    let (directories, _fields) = scan_paths(config, input_directory)?;
+    let (directories, files) = scan_paths(config, input_directory)?;
+    populate_directory_table(package, &directories)?;
+    populate_file_table(package, &files)?;
 
+    Ok(())
+}
+
+fn populate_directory_table(
+    package: &mut Msi,
+    directories: &Vec<Directory>,
+) -> Result<(), MsiError> {
     create_directory_table(package)?;
 
     let query = Insert::into("Directory").rows(
@@ -57,6 +66,36 @@ pub(crate) fn add_paths(
     if let Err(err) = package.insert_rows(query) {
         return Err(MsiError::nested("Failed to insert row into table", err));
     };
+
+    Ok(())
+}
+
+fn populate_file_table(
+    package: &mut Msi,
+    files: &Vec<File>,
+) -> Result<(), MsiError> {
+    create_file_table(package)?;
+    unimplemented!();
+
+    // let query = Insert::into("File").rows(
+    //     files
+    //         .iter()
+    //         .map(|file| {
+    //             vec![
+    //                 Value::from(file.id().to_string()),
+    //                 match &dir.parent_id() {
+    //                     Some(p) => Value::from(p.to_string()),
+    //                     None => Value::Null,
+    //                 },
+    //                 Value::from(dir.name().to_string()),
+    //             ]
+    //         })
+    //         .collect(),
+    // );
+
+    // if let Err(err) = package.insert_rows(query) {
+    //     return Err(MsiError::nested("Failed to insert row into table", err));
+    // };
 
     Ok(())
 }
@@ -185,7 +224,38 @@ fn create_directory_table(package: &mut Msi) -> Result<(), MsiError> {
     );
 
     if let Err(e) = result {
-        let err = error!("Failed to create directory table: {}", e);
+        let err = error!("Failed to create Directory table: {}", e);
+        return Err(MsiError::nested(err, Box::new(e)));
+    }
+
+    Ok(())
+}
+
+fn create_file_table(package: &mut Msi) -> Result<(), MsiError> {
+    let result = package.create_table(
+        "File",
+        vec![
+            Column::build("File").primary_key().id_string(72),
+            Column::build("Component_").id_string(72),
+            Column::build("FileName")
+                .category(Category::Filename)
+                .string(255),
+            Column::build("FileSize").int32(), // Marked as DoubleInteger
+            Column::build("Version")
+                .nullable()
+                .category(Category::Version)
+                .string(72),
+            Column::build("Language")
+                .nullable()
+                .category(Category::Language)
+                .string(20),
+            Column::build("Attributes").nullable().int16(),
+            Column::build("Sequence").int16(),
+        ],
+    );
+
+    if let Err(e) = result {
+        let err = error!("Failed to create File table: {}", e);
         return Err(MsiError::nested(err, Box::new(e)));
     }
 
